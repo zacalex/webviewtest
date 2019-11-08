00df0a11356d0e29f1031996348f7e3bbf5e2ab9
Calvin Huang <calhuang@google.com>
2019-11-07 19:58:51 -0800

    android webview

--- app/src/main/java/com/google/andriod/compactexample/WebViewActivity.kt
+++ app/src/main/java/com/google/andriod/compactexample/WebViewActivity.kt
@@ -11,85 +11,18 @@ import android.annotation.SuppressLint
 import android.view.View
 import im.delight.android.webview.AdvancedWebView
 
-class WebViewActivity : AppCompatActivity(), AdvancedWebView.Listener {
+class WebViewActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.web_view)
 
+        webview.settings.javaScriptEnabled = true
 
         val url = intent.getStringExtra("url")
-        webview.setListener(this, this)
-        webview.setDesktopMode(true)
-        webview.setGeolocationEnabled(false)
-        webview.setMixedContentAllowed(true)
-        webview.setCookiesEnabled(true)
-        webview.setThirdPartyCookiesEnabled(true)
-        webview.addHttpHeader("X-Requested-With", "");
 
         webview.loadUrl(url)
     }
 
-    @SuppressLint("NewApi")
-    override fun onResume() {
-        super.onResume()
-        webview.onResume()
-        // ...
-    }
-
-    @SuppressLint("NewApi")
-    override fun onPause() {
-        webview.onPause()
-        // ...
-        super.onPause()
-    }
-
-    override fun onDestroy() {
-        webview.onDestroy()
-        // ...
-        super.onDestroy()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intent)
-        webview.onActivityResult(requestCode, resultCode, intent)
-        // ...
-    }
-
-    override fun onBackPressed() {
-        if (!webview.onBackPressed()) {
-            return
-        }
-        // ...
-        super.onBackPressed()
-    }
-
-
-    override fun onPageError(errorCode: Int, description: String, failingUrl: String) {
-
-    }
-
-    override fun onDownloadRequested(
-        url: String,
-        suggestedFilename: String,
-        mimeType: String,
-        contentLength: Long,
-        contentDisposition: String,
-        userAgent: String
-    ) {
-
-
-    }
-
-    override fun onExternalPageRequest(url: String) {
-
-    }
-
-    override fun onPageFinished(url: String?) {
-    }
-
-    override fun onPageStarted(url: String?, favicon: Bitmap?) {
-        webview.visibility = View.VISIBLE
-    }
 }
